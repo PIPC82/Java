b4c676636ef1a123c895c01882cebc6c9f2979a5
Vasja <voitenco@gmail.com>
2022-03-26 23:43:29 +0200

    first commit

---
+++ HelloWorld/src/ua/com/voitenco/Hello.java
@@ -0,0 +1,15 @@
+package ua.com.voitenco;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("Взлом Пентагона:");
+         for (int i=0; i<100; i=i+10)
+         {
+        	 System.out.println(i+" %");
+         }
+         System.out.println("Пентагон взломан!!!");
+	}
+
+}
