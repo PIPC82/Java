8b5cadcb08cb9894d7980cffc5fd507578dd39a4
Vasja <voitenco@gmail.com>
2022-03-27 17:30:22 +0300

    UTf-8

--- HelloWorld/src/ua/com/voitenco/Hello.java
+++ HelloWorld/src/ua/com/voitenco/Hello.java
@@ -4,12 +4,12 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         System.out.println("Взлом Пентагона:");
+         System.out.println("Р—Р»Р°РјР°С‚Рё РџРµРЅС‚Р°РіРѕРЅ:");
          for (int i=0; i<100; i=i+10)
          {
         	 System.out.println(i+" %");
          }
-         System.out.println("Пентагон взломан 2 !!!!!!!");
+         System.out.println("РџРµРЅС‚Р°РіРѕРЅ РІР·Р»РѕРјР°РЅ!");
 	}
 
 }
